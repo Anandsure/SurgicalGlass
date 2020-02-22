868c8a442cb5a77dad67c52bb35eba45ec7ea473
unknown-guy-1610 <wwwanandsuresh@gmail.com>
2020-02-22 18:23:54 +0530

    added gtts

--- speech_trial.py
+++ speech_trial.py
@@ -1,42 +1,55 @@
-import azure.cognitiveservices.speech as speechsdk
+import analyze as az
+#import speech_trial as stt
+from gtts import gTTS
+import speech_recognition as sr
+import os
+import re
+import webbrowser
+import smtplib
+import requests
+'''import pyttsx3
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.runAndWait()'''
 
 
-# Creates an instance of a speech config with specified subscription key and service region.
-# Replace with your own subscription key and service region (e.g., "westus").
-speech_key, service_region = "008eed6eb82346a7a07ac66369b076bf", "centralndia"
-speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+def myCommand():
+   
 
-# Creates a recognizer with the given settings
-speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
+    r = sr.Recognizer()
 
-#print("Say something...")
+    with sr.Microphone() as source:
+        print('Ready...')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+
+    try:
+        command = r.recognize_google(audio).lower()
+        print('You said: ' + command + '\n')
 
 
-def rec():
-    print("Say something...")
-    result = speech_recognizer.recognize_once()
-    # Checks result.
-    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
-        #print("Recognized: {}".format(result.text))
-        z = ''
-        for i in result.text:
-            if i != '.':
-                z+=i
-            else:
-                continue
+    except sr.UnknownValueError:
+        print('Your last command couldn\'t be heard')
+        command = myCommand()
 
-        return z
-    elif result.reason == speechsdk.ResultReason.NoMatch:
-        print("No speech could be recognized: {}".format(result.no_match_details))
-        return 0
-    elif result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = result.cancellation_details
-        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+    return command
 
-        if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            print("Error details: {}".format(cancellation_details.error_details))
-            return 0
+    
 
-if __name__ == '__main__':
-    text=  rec()
+
+def get_text():
+    text = myCommand()
+    SUBSCRIPTION_KEY_ENV_NAME = "e0a4ec68847644849409dce0a433d785"
+
+   # text =  stt.rec()
+    #text = 'can someone pass the dissection sissors?'
     print(text)
+
+    keys = az.key_phrases(SUBSCRIPTION_KEY_ENV_NAME,text)
+    ent = az.entity_extraction(SUBSCRIPTION_KEY_ENV_NAME,text)
+    print(keys)
+
+    return [keys,ent]
+
+get_text()
